ba1e7baa99c41d312441eedfd5d7ce7eb5c0b4a0
floyda <andyfloyd86@gmail.com>
2020-04-05 21:43:10 +0000

    Mount graphql api

--- app/main.py
+++ app/main.py
@@ -3,41 +3,42 @@ main.py
 """
 # from ariadne.asgi import GraphQL
 # from api.graphql.api import schema
-import asyncio
-from ariadne import SubscriptionType, make_executable_schema
+# import asyncio
+# from ariadne import SubscriptionType, make_executable_schema
 from ariadne.asgi import GraphQL
 
-type_def = """
-    type Query {
-        _unused: Boolean
-    }
+# type_def = """
+#     type Query {
+#         _unused: Boolean
+#     }
 
-    type Subscription {
-        counter: Int
-    }
-"""
+#     type Subscription {
+#         counter: Int
+#     }
+# """
 
-subscription = SubscriptionType()
+# subscription = SubscriptionType()
 
-# @subscription.source("counter")
-async def counter_generator(obj, info):
-    yield 1
-    # for i in range(5):
-    #     await asyncio.sleep(1)
-    #     yield i
-
-
-# @subscription.field("counter")
-def counter_resolver(count, info):
-    return info
+# # @subscription.source("counter")
+# async def counter_generator(obj, info):
+#     yield 1
+#     for i in range(5):
+#         await asyncio.sleep(1)
+#         yield i
 
 
-subscription.set_field("counter", counter_resolver)
-subscription.set_source("counter", counter_generator)
-schema = make_executable_schema(type_def, subscription)
+# # @subscription.field("counter")
+# def counter_resolver(count, info):
+#     return info
+
+
+# subscription.set_field("counter", counter_resolver)
+# subscription.set_source("counter", counter_generator)
+# schema = make_executable_schema(type_def, subscription)
 
 from fastapi import FastAPI
 from routers import locations, login
+from api.graphql.api import schema
 
 app = FastAPI()
 
