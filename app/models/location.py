684477d4452ad73dc12e6d4b16ed86e421374359
floyda <andyfloyd86@gmail.com>
2020-05-19 19:04:23 +0000

    location change

--- app/models/location.py
+++ app/models/location.py
@@ -1,10 +1,19 @@
 from typing import List
 from pydantic import BaseModel
 
+class Profile(BaseModel):
+    ame: str
+    profileType: str
+    # parameters: List[Parameter]
+    # timespans: List[Timespans]
+
 class Location(BaseModel):
-    siteid: str
-    description: str
-    client: str
+    name: str
+    siteCode: str
+    siteName: str
+    siteDescription: str
+    siteClient: str
+    profiles: List[Profile]
 
 class Locations(BaseModel):
     locations: List[Location]
